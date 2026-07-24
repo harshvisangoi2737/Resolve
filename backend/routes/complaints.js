5c4de3ba37f1da4c681b54615cef3a37b7540452
harshvisangoi2737 <harshvisangoi2737@gmail.com>
2026-07-24 17:09:55 +0530

    Update all backend filesFix model file casing for Linux

--- backend/routes/complaints.js
+++ backend/routes/complaints.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Complaint = require('../models/Complaint');
+const Complaint = require('../models/complaint');
 
 // POST - Create new complaint
 router.post('/', async (req, res) => {
